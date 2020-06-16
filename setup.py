c13379cbeb91b43a84828f3ca06c435c30bf2447
sralloza <sralloza@gmail.com>
2020-06-16 22:52:53 +0200

    feat: add console scripts

--- setup.py
+++ setup.py
@@ -6,4 +6,10 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "backup-to-cloud=backup_to_cloud.main:main",
+            "btc=backup_to_cloud.main:main",
+        ],
+    },
 )
